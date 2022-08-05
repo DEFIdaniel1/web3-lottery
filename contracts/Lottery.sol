b6d4d3a670cee03551f99a328fadf1e1563bb747
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-08-05 12:19:21 -0600

    added getter functions

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -178,4 +178,12 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
     function getRequestConfirmations() public pure returns (uint256) {
         return REQUEST_CONFIRMATIONS;
     }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getLatestTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
 }
