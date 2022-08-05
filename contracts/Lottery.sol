3e28a4e3bf3962a646276c32a109ae0ced2153d3
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-08-05 12:16:21 -0600

    Lottery.sol contract completed. Chainlink VRF and Keepers added for random number and immutable time intervals

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,7 +1,3 @@
-//enter the lottery
-//pick random winner (verifiable)
-//winner selected every X minutes (fully automated)
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
@@ -12,13 +8,25 @@ import '@chainlink/contracts/src/v0.8/KeeperCompatible.sol'; //imports BOTH comp
 error Lottery__SendMoreETH();
 error Lottery__TransferFailed();
 error Lottery__LotteryNotOpen();
+error error_LotteryUpkeepNotNeeded(
+    uint256 currentBalance,
+    uint256 numPlayers,
+    uint256 lotteryState
+);
+
+/**
+ *   @title A sample lottery contract
+ *   @author DEFIdaniel
+ *   @notice This contract is for an untamperable, decentralized contract
+ *   @dev This implements Chainlink VRF v2 and Chainlink Keepers
+ */
 
 contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
     // Type declarations
     enum LotteryState {
         OPEN,
         CALCULATING
-    } //is creating a uint256 0=open, 1=calculating
+    }
 
     // State Variables
     uint256 private immutable i_entranceFee;
@@ -27,10 +35,10 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
     bytes32 private immutable i_gasLane;
     uint64 private immutable i_subscriptionId;
     uint32 private immutable i_callbackGasLimit;
+    uint256 private immutable i_interval;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
-
-    //Lottery Variables
+    uint256 private s_lastTimeStamp;
     address private s_recentWinner;
     LotteryState private s_lotteryState;
 
@@ -39,12 +47,14 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
     event RequestedLotteryWinner(uint256 indexed requestId);
     event WinnerPicked(address indexed winner);
 
+    // Functions
     constructor(
         address vrfCoordinatorV2,
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
@@ -52,6 +62,8 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
         s_lotteryState = LotteryState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     // Functions
@@ -67,18 +79,46 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
         emit LotteryEnter(msg.sender);
     }
 
-    function checkUpKeep(
-        bytes calldata /*checkData*/
-    ) external override {
+    /**
+     *   @dev This function uses chainlink keeper node calls
+     *   When it returns true, can execute the lottery function
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
         bool isOpen = (LotteryState.OPEN == s_lotteryState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = address(this).balance > 0;
+        // IF all these are met below, it's time to run the lottery
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+        return (upkeepNeeded, '0x0');
     }
 
-    //2 steps for contract fulfillment to make it more secure
-    //Step1: requestRandomWords (number); Step2: use number to choose a winner
-
-    function requestRandomWinner() external {
+    /**
+     * @dev Once `checkUpkeep` is returning `true`, this function is called
+     * and it kicks off a Chainlink VRF call to get a random winner.
+     */
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep(''); //pass blank calldata
+        if (!upkeepNeeded) {
+            revert error_LotteryUpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_lotteryState)
+            );
+        }
         s_lotteryState = LotteryState.CALCULATING;
-        //list of params required by chainlink function
         uint256 s_requestId = i_vrfCoordinator.requestRandomWords( //requestID stores a lot of the request's info for use
             i_gasLane, //aka keyHash in docs - gives max price you'll pay for gas
             i_subscriptionId, //subscription for contract funding the request
@@ -89,7 +129,9 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
         emit RequestedLotteryWinner(s_requestId);
     }
 
-    // function from the chainlink contract we're calling
+    /**
+     *   @dev Function the Chainlink VRF calls to send money to a random winner
+     */
     function fulfillRandomWords(
         uint256, /*requestID*/
         uint256[] memory randomWords //not providing requestID
@@ -102,6 +144,7 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
         s_recentWinner = recentWinner;
         s_lotteryState = LotteryState.OPEN;
         s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
 
         //payout
         (bool success, ) = recentWinner.call{value: address(this).balance}(''); //sends all contract value to the recent winner
@@ -122,5 +165,17 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
 
     function getRecentWinner() public view returns (address) {
         return s_recentWinner;
+    }
+
+    function getLotteryState() public view returns (LotteryState) {
+        return s_lotteryState;
+    }
+
+    function getNumWords() public pure returns (uint256) {
+        return NUM_WORDS;
+    }
+
+    function getRequestConfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
     }
 }
